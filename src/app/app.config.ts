762983f77eaaed9e54ce5574ccbfd39bf13df99b
ac484 <ac7x7x@gmail.com>
2025-08-01 18:03:04 +0800

    更新 reCAPTCHA Enterprise 提供者的站點金鑰，以增強安全性和驗證功能。

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -78,7 +78,7 @@ export const appConfig: ApplicationConfig = {
   providers: providers,
   providers: [provideFirebaseApp(() => initializeApp({ projectId: "ng-acc", appId: "1:289956121604:web:4dd9d608a2db962aeaf951", storageBucket: "ng-acc.firebasestorage.app", apiKey: "AIzaSyCmWn3NJBClxZeJHsg-eaEaqA3bdB9bzOQ", authDomain: "ng-acc.firebaseapp.com", messagingSenderId: "289956121604", measurementId: "G-6YM5S9LCNV" })), provideAuth_alias(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  const provider = new ReCaptchaEnterpriseProvider("6LdMz5YrAAAAAJE130XrD8SxJ3Ijn2ZATV-BQQwo");
   return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
 }), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())]
 };
