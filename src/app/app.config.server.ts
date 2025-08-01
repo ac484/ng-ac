c5c1f6627aff15f19aaee2f3393ffbaabd3b2dd8
ac484 <ac7x7x@gmail.com>
2025-08-01 17:23:13 +0800

    移除 package-lock.json，更新 package.json 以新增 @angular/fire 及 firebase 依賴，並調整其他依賴項目；在 app.config.server.ts 中新增提供 HTTP 客戶端的配置以啟用 fetch。

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,16 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
+import { provideHttpClient, withFetch } from '@angular/common/http';  // <-- 新增這行
+
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
+    provideServerRouting(serverRoutes),
+    provideHttpClient(withFetch()),   // <-- 加上這行，啟用 fetch
   ]
 };
 
