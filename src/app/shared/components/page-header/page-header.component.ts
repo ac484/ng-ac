284bea6b60d6351c7b145fa22243ffd81299d348
ac484 <ac7x7x@gmail.com>
2025-08-02 18:39:35 +0800

    1

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzOutletModule } from 'ng-zorro-antd/core/outlet';
+import { NzSafeAny } from 'ng-zorro-antd/core/types';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+
+export interface PageHeaderType {
+  title: string;
+  desc: string | TemplateRef<NzSafeAny>;
+  extra: string | TemplateRef<NzSafeAny>;
+  breadcrumb: string[];
+  footer: string | TemplateRef<NzSafeAny>;
+}
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NzPageHeaderModule, NzBreadCrumbModule, NzOutletModule]
+})
+export class PageHeaderComponent implements OnInit {
+  @Input() backTpl: TemplateRef<NzSafeAny> | undefined;
+  @Input() pageHeaderInfo: Partial<PageHeaderType> = {};
+  @Input() backUrl = '';
+
+  constructor(private router: Router) {}
+
+  back(): void {
+    this.router.navigateByUrl(this.backUrl);
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,4 +1,11 @@
 // Components
+export * from './components/ant-table/ant-table.component';
+export * from './components/tree-table/tree-table.component';
+export * from './components/card-table-wrap/card-table-wrap.component';
+
+// Pipes
+export * from './pipes/map.pipe';
+export * from './pipes/table-filed.pipe';
 
 // Utils
 export * from './utils/yuan';
