0cca67474bf52ae487789b8708904511f105e5eb
𓂺 <ac7x7x@gmail.com>
2025-08-05 21:25:25 +0800

    移除多個不再使用的檔案，包括 README 文件及組件，簡化專案結構以提升可維護性與可讀性。

--- src/app/interface/components/transaction/transaction-form.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-transaction-form',
-  standalone: true,
-  template: `<div>Transaction Form Component</div>`
-})
-export class TransactionFormComponent {}
